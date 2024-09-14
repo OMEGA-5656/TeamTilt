c26114e27fe12babc5ba236fc1a31f881b2c723e
OMEGA-5656 <abhisheksaini642794@gmail.com>
2024-09-14 13:46:06 +0530

    project initialized

---
+++ android/src/main/java/com/newgame/teamtilt/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.newgame.teamtilt.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.newgame.teamtilt.TeamTiltMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new TeamTiltMain(), configuration);
+    }
+}
