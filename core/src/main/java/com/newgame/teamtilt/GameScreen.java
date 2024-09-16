5dbe7650b2ce30f86d5a577556438e1e52972673
OMEGA-5656 <abhisheksaini642794@gmail.com>
2024-09-16 14:46:59 +0530

    add platform

--- core/src/main/java/com/newgame/teamtilt/GameScreen.java
+++ core/src/main/java/com/newgame/teamtilt/GameScreen.java
@@ -4,26 +4,42 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
     final TeamTiltMain game;
     private Texture backgroundTexture;
     private Texture characterTexture;
+    private Texture platformTexture;
 
-    // Character position
+    // Character position and physics variables
     private float characterX;
     private float characterY;
+    private float velocityY = 0;
+    private final float gravity = -500f;
+    private boolean isGrounded = false;
+
+    // Platform
+    private Rectangle platform;
 
     public GameScreen(final TeamTiltMain game) {
         this.game = game;
 
-        // Load the background and character textures
+        // Load the background, character, and platform textures
         backgroundTexture = new Texture(Gdx.files.internal("backgrounds/background.png"));
         characterTexture = new Texture(Gdx.files.internal("characters/character.png"));
+        platformTexture = new Texture(Gdx.files.internal("platforms/platform.png"));
 
         // Set initial character position (centered on screen)
         characterX = (Gdx.graphics.getWidth() - characterTexture.getWidth()) / 2;
         characterY = (Gdx.graphics.getHeight() - characterTexture.getHeight()) / 2;
+
+        // Set platform size and position
+        platform = new Rectangle();
+        platform.x = 100; // X-position of platform
+        platform.y = 100; // Y-position of platform
+        platform.width = 300; // Width of platform
+        platform.height = 20; // Height of platform
     }
 
     @Override
@@ -35,9 +51,25 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Draw the background and character
+        // Apply gravity if not grounded
+        if (!isGrounded) {
+            velocityY += gravity * delta;
+            characterY += velocityY * delta;
+        }
+
+        // Collision detection with platform
+        if (characterY <= platform.y + platform.height && characterX + characterTexture.getWidth() > platform.x && characterX < platform.x + platform.width) {
+            characterY = platform.y + platform.height; // Snap character to platform
+            velocityY = 0; // Stop vertical movement
+            isGrounded = true;
+        } else {
+            isGrounded = false; // Allow falling if not on platform
+        }
+
+        // Draw the background, platform, and character
         game.batch.begin();
         game.batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // Draw background
+        game.batch.draw(platformTexture, platform.x, platform.y, platform.width, platform.height); // Draw platform
         game.batch.draw(characterTexture, characterX, characterY); // Draw character
         game.batch.end();
     }
@@ -59,5 +91,6 @@ public class GameScreen implements Screen {
         // Dispose of the textures to prevent memory leaks
         backgroundTexture.dispose();
         characterTexture.dispose();
+        platformTexture.dispose();
     }
 }
